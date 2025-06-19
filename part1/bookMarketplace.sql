45780532a42dcc2626f58e63fd1b52bdbf1cc03a
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-19 06:41:37 +0000

    [automatic commit] 2025-06-19 06:41:37 UTC 129.127.202.43

--- part1/bookMarketplace.sql
+++ part1/bookMarketplace.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS BookMarketplace;
 CREATE DATABASE BookMarketplace;
 USE BookMarketplace;
 
-CREATE TABLE Location (
+CREATE TABLE Locations (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR (50) NOT NULL UNIQUE,
     email VARCHAR (100) NOT NULL UNIQUE,
