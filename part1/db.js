ab66f842e62b46a2a450bb62400a2a7061248ad4
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-19 11:02:09 +0000

    [automatic commit] 2025-06-19 11:02:11 UTC 14.2.23.199

--- part1/db.js
+++ part1/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
+const db = mysql.createPool({
+    host: '127.0.0.1',
     user: 'root',
     password: 'mypassword',
     database: 'textbook_marketplace'
 });
-const
+
 module.exports = db;
