231459a0c558afec75b04fd24aa5034c2f7b955a
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-19 10:02:20 +0000

    [automatic commit] 2025-06-19 10:02:20 UTC 14.2.23.199

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -9,6 +9,7 @@ router.get('/items', async (req,res)=>{
     const[rows] = await db.query(`
         SELECT bl.BookID, bi.Title, u.Name AS SellerName
         FROM BookListings bl
-        JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoIDJOI
+        JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
+        JOIN Users u ON bl.SellerID = u.UserID
         `);
 });
