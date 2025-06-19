bdc7f89cdefa633b96632c10bbe84d673a0661f8
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-19 11:01:33 +0000

    [automatic commit] 2025-06-19 11:01:33 UTC 14.2.23.199

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,14 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-    host: '/var/run/mysqld/mysqld.sock',
+code: 'ER_ACCESS_DENIED_ERROR',
+  errno: 1045,
+  sql: undefined,
+  sqlState: '28000',
+  sqlMessage: "Access denied for user 'root'@'localhost' (using password: YES)"
+}
+
+    host: '127.0.0.1',
     user: 'root',
     password: 'mypassword',
     database: 'textbook_marketplace'
