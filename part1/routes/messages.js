326d69e947bee47384cb03069331dc7f73e16ee1
Natsumi Baba <a1939721@adelaide.edu.au>
2025-06-19 12:24:48 +0000

    [automatic commit] 2025-06-19 12:24:48 UTC 14.2.23.199

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -17,7 +17,7 @@ router.get('/items', async (req,res) => {
 
 router.get('/messages', async (req, res) => {
     const [rows] = await db.query(`
-        SELECT m.MESSAGE, m.SentAt, bi.Title, u.Name AS BuyerName
+        SELECT m.MESSAGE AS MessageText, m.SentAt, bi.Title, u.Name AS BuyerName
         FROM messages m
         JOIN BookListings bl ON m.BookID = bl.BookID
         JOIN BookInfo bi ON bl.BookInfoID = bi.BookInfoID
